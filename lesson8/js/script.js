79507b6b2b75855055ce0438abeb349226591dba
Nikolay <joker19121992@mail.ru>
2018-07-02 00:28:39 +0500

    Add files via upload

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -35,11 +35,11 @@ function showTabContent(b) {
 
 //timer 
 	
-	let deadline = '2018-03-20';
+	let deadline = '2018-08-02';
 
 	function getTimeRemaining(endtime) {
 		let t = Date.parse(endtime) - Date.parse(new Date()),
-		second = Math.floor((t/1000) % 60),
+		seconds = Math.floor((t/1000) % 60),
 		minutes = Math.floor((t/1000/60) % 60),
 		hours = Math.floor((t/(1000*60*60)) );
 
@@ -47,33 +47,35 @@ function showTabContent(b) {
 			'total': t,
 			'hours': hours,
 			'minutes': minutes,
-			'second': second 
+			'seconds': seconds 
 		};
 	};
 
+	
+
 	function setClock(id, endtime) {
 		let timer = document.getElementById(id),
-			hours = timer.querySelector('hours'),
-			minutes = timer.querySelector('minutes'),
-			second = timer.querySelector('second');
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds');
+
+		function updateClock() {
+			let t = getTimeRemaining(endtime);
+				hours.innerHTML = t.hours; 
+				minutes.innerHTML = t.minutes; 
+				seconds.innerHTML = t.seconds; 
+
 			
-			function updateClock() {
-				let t = getTimeRemaining(endtime);
-				hours.innerHTML = t.hours;
-				minutes.innerHTML = t.minutes;
-				second.innerHTML = t.second;
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
 
-				if (t.total <= 0) {
-					clearInterval(timeInterval);
-				}
+
 			};
-			updateClock();
-			let timeInterval = setInterval(updateClock, 1000);
+		};
+		updateClock();
+		let timeInterval = setInterval(updateClock, 1000);
 	};
-
-
 	setClock('timer', deadline);
-
 });
 
 
